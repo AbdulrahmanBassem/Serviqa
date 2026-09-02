36a02bc8d7bfefafdbc219395a4b6937dab524ea
AbdulrahmanBassem <abdulrahmanbassem@outlook.com>
2026-09-02 23:31:10 +0300

    main

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,17 @@ import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./config/queryClient";
+import { GlobalErrorBoundary } from "./components/errors/GlobalErrorBoundary";
 import App from "./App.tsx";
 import "./styles/variables.css";
-// import "./styles/index.css"; // (Uncomment when you add global styles)
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <GlobalErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </GlobalErrorBoundary>
   </StrictMode>
 );
